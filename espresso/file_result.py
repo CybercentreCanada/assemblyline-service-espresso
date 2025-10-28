73df0437b0f58fcac32bce9b216a3569692166ff
Shelly Wang <ching-wen.wang@cyber.gc.ca>
2025-10-28 19:35:54 +0000

    change type for python 3.9

--- espresso/file_result.py
+++ espresso/file_result.py
@@ -25,12 +25,18 @@ class FileResult():
     embedded_pes: bool
     launchable_file: bool
     interesting_class_attributes: ClassAttributes
-    header_hex: str | None
+    header_hex: str
     extracted_class_file: Tuple
     empty_file: bool
 
     @staticmethod
     def empty_file_result(file_path):
-        return FileResult(file_path=file_path, embedded_pes=False, launchable_file=False,
-                          interesting_class_attributes=ClassAttributes.empty_class_attributes(),
-                          header_hex=None, extracted_class_file=(None, None, None), empty_file=False)
+        return FileResult(
+            file_path=file_path,
+            embedded_pes=False,
+            launchable_file=False,
+            interesting_class_attributes=ClassAttributes.empty_class_attributes(),
+            header_hex="",
+            extracted_class_file=(None, None, None),
+            empty_file=False,
+        )
